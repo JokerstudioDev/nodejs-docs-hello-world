083a6f0569b2c9bb375af0382adca5f98b0211d1
JokerstudioDev <jokerstudio@hotmail.com>
2017-09-03 19:23:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
+var data = require('./data')
 
 app.use(bodyParser.json())
 
 //วิธีกำหนด routing หรือ API endpoint
 app.get('/', function (req, res) {
   res.send('API Started')
+})
+
+app.get('/accounts', function (req, res) {
+  res.json(data.accounts)
+})
+
+app.get('/account/:id', function (req, res) {
+  var account = data.accounts.filter(acc => acc.account_id === req.params.id);
+  res.json(account)
+})
+
+app.post('/account/add', function (req, res) {
+  data.accounts.push(req.body)
+  res.json(req.body)
 })
 
 var port = process.env.PORT || 1337;
