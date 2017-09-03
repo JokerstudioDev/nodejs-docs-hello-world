cf7692259fd7fc0093e323b14185442d3f33c89e
JokerstudioDev <jokerstudio@hotmail.com>
2017-09-03 19:20:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.json())
+
 app.get('/', function (req, res) {
   res.send('API Started')
 })
